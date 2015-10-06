b634ebec0fea77913b638138c3e68ee9d182c700
Erik Stockmeier <erik.d.stock@gmail.com>
2015-10-05 22:18:46 -0500

    add js

---
+++ divvy_down.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+	console.log('ready');
+
+	// var divvyBase = new Firebase('https://divvy-flake.firebaseio.com/')
+	// if db is older than one minute, regenerate it
+	findNearby();
+
+});
+
+function findNearby() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showStations);
+  } else {
+  	debugger;
+    $('ul#station-list').prepend("<li>Geolocation is not supported by this browser.</li>");
+  }
+};
+
+function showStations (position) {
+	getStations(position);
+	debugger;
+}
+
+function getStations (position) {
+	$.ajax({
+		method: "get",
+		url: "http://www.divvybikes.com/stations/json/"
+	})
+
+}
