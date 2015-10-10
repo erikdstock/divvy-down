42e03bfeb8364d22760da2312befa67e72aaa57e
Erik Stockmeier <erik.d.stock@gmail.com>
2015-10-10 15:21:48 -0500

    basic running express server, no firebase integration yet

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	var stations;
 	// console.log(req);
 	// console.log(next);
+	$.get("https://www.divvybikes.com/stations/json").done(function(data){
+		console.log(data);
+		stations = data;
+	})
   res.render('index', { title: 'DivvyDowner', stations: (typeof(stations) !== 'undefined' ? stations : []) });
 });
 
